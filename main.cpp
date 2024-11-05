56ce14fbc2107f307f8e741b7620d0a7a7752a17
Marcial Trujillo <marcialtrujillo.1234@gmail.com>
2024-11-05 10:56:11 -0400

    add: node class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
-
 using namespace std;
+
+template<class T>
+class Node{
+    private:
+    T data;
+    Node<T>* next = nullptr;
+    Node<T>* prev = nullptr;
+    public:
+    T getData();
+    void setData(T);
+    Node<T>* getNext();
+    void setNext(Node<T>*);
+    Node<T>* getPrev();
+    void setPrev(Node<T>*);
+};
 
 int main(){
 
