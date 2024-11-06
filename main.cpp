afba3613a499a837ca992bd9e5f0c2ae39efb841
Marcial Trujillo <marcialtrujillo.1234@gmail.com>
2024-11-06 10:41:21 -0400

    add: queue class

--- main.cpp
+++ main.cpp
@@ -16,6 +16,19 @@ class Node{
     void setPrev(Node<T>*);
 };
 
+template<class T>
+class Queue{
+    private:
+    Node<T>* head = nullptr;
+    Node<T>* tail = nullptr;
+    public:
+    Queue(T,Node<T>*);
+    ~Queue();
+    void print();
+    bool isEmpty();
+    int getLength();
+};
+
 int main(){
 
 }
