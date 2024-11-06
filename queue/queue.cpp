2eac4857734ffa84755f521f0838bb2b639eb12f
Marcial Trujillo <marcialtrujillo.1234@gmail.com>
2024-11-06 19:52:41 -0400

    add: push function

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -19,3 +19,15 @@ Queue<T>::~Queue(){
     this->head = nullptr;
     this->tail = nullptr;
 }
+
+template<class T>
+void Queue<T>::push(T data){
+    Node<T> *node = new Node<T>(data,nullptr);
+    if(isEmpty()){
+        head = node;
+        tail = node;
+        return
+    }
+    this->tail->setNext(node);
+    this->tail = node;
+}
