15c290819a8418bbabee84f57d347b02c49c1faa
Marcial Trujillo <marcialtrujillo.1234@gmail.com>
2024-11-08 17:11:45 -0400

    add: stack functions

---
+++ stack/stack.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include"./stack.h"
+using namespace std;
+
+template<class T>
+Stack<T>::Stack(T data,Node<T>* node){
+    Node<T> *newNode = new Node<T>(data,node);
+    head = newNode;
+}
+
+template<class T>
+bool Stack<T>::isEmpty(){
+    return this->head==NULL;
+}
+
+template<class T>
+Stack<T>::~Stack(){
+    this->head= nullptr;
+}
+
+template<class T>
+void Stack<T>::push(T data){
+    
+}
