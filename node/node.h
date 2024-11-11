c7ca85bcc1ac898cdb000a1b40c01243774452d6
Marcial Trujillo <marcialtrujillo.1234@gmail.com>
2024-11-11 15:02:05 -0400

    fix: node class

--- node/node.h
+++ node/node.h
@@ -6,6 +6,7 @@ class Node{
     private:
     T data;
     Node<T>* next = nullptr;
+    Node<T>* prev = nullptr;
     public:
     Node(T,Node<T>*);
     ~Node();
@@ -13,5 +14,7 @@ class Node{
     void setData(T);
     Node<T>* getNext();
     void setNext(Node<T>*);
+    Node<T>* getPrev();
+    void setPrev(Node<T>*);
 };
 #endif
