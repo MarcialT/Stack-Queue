6d2121f51d917cd606068aca91bb8fbabe6fdf40
MarcialT <Marcialtrujillo.1234@gmail.com>
2024-11-22 20:52:24 -0400

    feat: pbp implementation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"./stack/stack.cpp"
+#include"./queue/queue.cpp"
 using namespace std;
 
 int main(){
@@ -9,4 +10,18 @@ miPila.push('s');
 miPila.push('t');
 cout<<"pila invertida"<<endl;
 miPila.revert();
+
+Queue<int> queue;
+queue.push(10);
+queue.push(20);
+queue.push(30);
+
+Node<int>* node = queue.getHead();
+if (node != nullptr) node->setPriority(1);
+node = node->getNext();
+if (node != nullptr) node->setPriority(3);
+node = node->getNext();
+if (node != nullptr) node->setPriority(2);
+queue.processByPriority();
+
 }
