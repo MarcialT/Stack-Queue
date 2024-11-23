ca6986a80569406c13814801efe4cc7724b5132b
MarcialT <Marcialtrujillo.1234@gmail.com>
2024-11-22 22:35:11 -0400

    feat: prioqueue implementation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include"./stack/stack.cpp"
 #include"./queue/queue.cpp"
+#include"./priorityqueue/priorityqueue.cpp"
 using namespace std;
 
 int main(){
@@ -10,12 +11,14 @@ miPila.push('s');
 miPila.push('t');
 cout<<"pila invertida"<<endl;
 miPila.revert();
+cout<<endl;
 
 Queue<int> queue;
 queue.push(10);
 queue.push(20);
 queue.push(30);
 
+//La prioridad mayor es el numero mas alto
 Node<int>* node = queue.getHead();
 if (node != nullptr) node->setPriority(1);
 node = node->getNext();
@@ -23,5 +26,16 @@ if (node != nullptr) node->setPriority(3);
 node = node->getNext();
 if (node != nullptr) node->setPriority(2);
 queue.processByPriority();
+cout<<endl;
 
+
+//La prioridad mayor es el numero mas bajo
+ priorityqueue<int> queue2;
+    queue2.push(10, 2);
+    queue2.push(5, 1);
+    queue2.push(20, 3);
+
+    while (!queue2.isEmpty()) {
+        cout << "Dato procesado: " << queue2.pop() <<endl;
+    }
 }
